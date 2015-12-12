e74294b54d53df7deeac6ff1e19d5e09a8bd87f1
Chase Stubblefield <chasestubblefield@gmail.com>
2015-12-11 18:35:37 -0800

    Add cases to test rounding for formatDuration()

--- format_duration_test.go
+++ format_duration_test.go
@@ -13,6 +13,10 @@ func TestFormatDuration(t *testing.T) {
 		{"1m", "1:00.00"},
 		{"1h", "1:00:00.00"},
 		{"1h1m1s", "1:01:01.00"},
+		{"12h34m56.789s", "12:34:56.79"},
+		{"0.999s", "0:01.00"},
+		{"1m59.999s", "2:00.00"},
+		{"1h59m59.999s", "2:00:00.00"},
 	}
 	for _, c := range cases {
 		d, err := time.ParseDuration(c.in)
